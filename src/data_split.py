d02059945902a70445170a737c48a465251d6151
masa-pocodigitalmarketing <masa@pocodigitalmarketing.com>
2024-05-21 19:40:35 +0900

    Update data_split.py

--- src/data_split.py
+++ src/data_split.py
@@ -10,8 +10,8 @@ def train_test_split(
     target_column_name: str,
 ) -> Tuple[pd.DataFrame, pd.Series, pd.DataFrame, pd.Series]:
     """ """
-    train_data = df[df.pickup_hour < cutoff_date].reset_index(drop=True)
-    test_data = df[df.pickup_hour >= cutoff_date].reset_index(drop=True)
+    train_data = df[pd.to_datetime(df.pickup_hour) < cutoff_date].reset_index(drop=True)
+    test_data = df[pd.to_datetime(df.pickup_hour) >= cutoff_date].reset_index(drop=True)
 
     X_train = train_data.drop(columns=[target_column_name])
     y_train = train_data[target_column_name]
